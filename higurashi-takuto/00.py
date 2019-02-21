bbd55a55becdc0121d2acbc5c2e206cd16724823
higurashi-takuto <takuto.8726.higurashi@gmail.com>
2019-02-21 12:29:43 +0900

    スライスを使って実装

---
+++ higurashi-takuto/00.py
@@ -0,0 +1,7 @@
+# 文字列"stressed"の文字を逆に（末尾から先頭に向かって）並べた文字列を得よ．
+
+string = 'stressed'
+# スライス[開始:終了:ステップ] 開始/終了は省略すると最初から最後まで。ステップはマイナスをつけると最後から
+string_backward = string[::-1]
+
+print(string_backward)
