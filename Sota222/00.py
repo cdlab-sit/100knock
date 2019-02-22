4ed051f6dca8f70873dac1f49c3db1c7c4a00feb
Sota222 <cy16179@shibaura-it.ac.jp>
2019-02-22 15:45:46 +0900

    first commit

---
+++ Sota222/00.py
@@ -0,0 +1,10 @@
+# 文字列"stressed"の文字を逆に（末尾から先頭に向かって）並べた文字列を得よ．
+
+str = 'stressed'
+
+# スライス[開始:終了:ステップ] 開始/終了は省略すると最初から最後まで。ステップはマイナスをつけると最後から
+string_backward = str[::-1]
+#test_str = str[1:3]
+test_str = str[-1]
+print(test_str)
+print(string_backward)
