7a84b91e3c86aff4c31173a403f24078d5fcc477
higurashi-takuto <takuto.8726.higurashi@gmail.com>
2019-02-23 23:54:44 +0900

    UNIXをコメントアウトに

--- higurashi-takuto/10.py
+++ higurashi-takuto/10.py
@@ -1,13 +1,9 @@
 # 行数をカウントせよ．確認にはwcコマンドを用いよ．
 
-import subprocess
-import path
-
 # Python
 with open('hightemp.txt', 'r') as f:
     # readline()は1行ずつ読むので、全部の行を読むにはreadlines()
     print(len(f.readlines()))
 
 # UNIX
-cmd = 'wc -l hightemp.txt'.split(' ')
-subprocess.run(cmd, cwd=path.current)
+# wc -l hightemp.txt
