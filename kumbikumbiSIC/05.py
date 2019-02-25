d8b694b57e6b1d98bf109ab1d96457eb8082bef5
Hikaru Morita <tamori.iitomo12@gmail.com>
2019-02-25 16:05:42 +0900

    05追加

---
+++ kumbikumbiSIC/05.py
@@ -0,0 +1,23 @@
+#リストからn-gramを作成
+def n_gram(words, n):
+	'''
+	n-gram
+
+	第一引数：リスト
+	第二引数：n値
+	戻り値：n-gramのリスト
+	'''
+	result = []
+	for i,word in enumerate(words):
+		result.append(words[i:i+n])
+
+	return result
+
+words = "I am an NLPer"
+list_words = words.split()
+
+word_bi_gram = n_gram(list_words, 2)
+print("単語bi-gram",word_bi_gram)
+
+char_bi_gram = n_gram(words, 2)
+print("文字bi-gram",char_bi_gram)
