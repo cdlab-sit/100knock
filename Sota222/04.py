020178f3b1fe6eae95e1274ec055d10541d6cb98
Sota222 <cy16179@shibaura-it.ac.jp>
2019-02-27 14:48:43 +0900

    コードを短縮、lambdaは後修正

--- Sota222/04.py
+++ Sota222/04.py
@@ -2,7 +2,6 @@
 # という文を単語に分解し，1, 5, 6, 7, 8, 9, 15, 16, 19番目の単語は先頭の1文字，それ以外の単語は先頭に2文字を取り出し，
 # 取り出した文字列から単語の位置（先頭から何番目の単語か）への連想配列（辞書型もしくはマップ型）を作成せよ．
 import re
-
 string = "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."
 
 
@@ -10,21 +9,11 @@ one_letter = [1, 5, 6, 7, 8, 9, 15, 16, 19]
 words = re.sub("[,.]", "", string).split(" ")
 
 chemical_symbols = {}
+
+test_lambda = lambda i, word: word[0] if i in one_letter else word[:2]
+
 for i, word in enumerate(words):
-    if i in one_letter:
-        chemical_symbols[word[0]] = i
-    else:
-        chemical_symbols[word[:2]] = i
+    chemical_symbols[test_lambda(i + 1, word)] = i
+
 
 print(chemical_symbols)
-
-
-
-
-
-
-
-
-
-
-#ans [H:1, He:2]....
