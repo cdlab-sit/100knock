145097fdbc86bd3599b0d9f0da9131d54e5ce0a6
Masaki-Okuyama <cy16114@shibaura-it.ac.jp>
2019-03-04 21:42:15 +0900

    8の解答追加

---
+++ octan/08.py
@@ -0,0 +1,20 @@
+# 与えられた文字列の各文字を，以下の仕様で変換する関数cipherを実装せよ．
+# *英小文字ならば(219 - 文字コード)の文字に置換
+# *その他の文字はそのまま出力
+# この関数を用い，英語のメッセージを暗号化・復号化せよ．
+
+
+def cipher(line):
+    ci_line = ""
+    for char in line:
+        if 96 < ord(char) < 123:
+            char = chr(219 - ord(char))
+        ci_line += char
+    return ci_line
+
+string = "abCdeFいろは"
+print("変化前: ", string)
+string = cipher(string)
+print("暗号化: ", string)
+string = cipher(string)
+print("復号化: ", string)
