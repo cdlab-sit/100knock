705682367adc035d7d7f0d6c4c28933a93f32aa6
Masaki-Okuyama <cy16114@shibaura-it.ac.jp>
2019-03-04 22:29:58 +0900

    9の解答追加

---
+++ octan/09.py
@@ -0,0 +1,18 @@
+# スペースで区切られた単語列に対して，各単語の先頭と末尾の文字は残し，それ以外の文字の順序をランダムに並び替えるプログラムを作成せよ．
+# ただし，長さが４以下の単語は並び替えないこととする．
+# 適当な英語の文（例えば"I couldn't believe that I could actually understand what I was reading : the phenomenal power of the human mind ."）を与え，その実行結果を確認せよ．
+import random
+
+string = "I couldn't believe that I could actually understand what I was"\
+    " reading : the phenomenal power of the human mind ."
+lis = string.split()
+rnd_list = []
+
+for word in lis:
+    if 4 < len(word):
+        word = word[0] + \
+            "".join(random.sample(word[1:-1], len(word[1:-1]))) + word[-1]
+    rnd_list.append(word)
+
+rnd_string = " ".join(rnd_list)
+print(rnd_string)
