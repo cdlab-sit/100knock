579eb41c50085d0ad22b35ccdc3319a4a493cd39
Sota222 <cy16179@shibauta-it.ac.jp>
2019-03-05 22:16:38 +0900

    pykakasiで漢字の読み方並び替えに対応

--- Sota222/17.py
+++ Sota222/17.py
@@ -3,17 +3,26 @@
 # python
 import sys
 import numpy as np
+from pykakasi import kakasi
 from os.path import dirname, join
+# 漢字を平仮名にするconvを作成
+kakasi = kakasi()
+kakasi.setMode('J', 'H')  # Japanese(漢字)をasciiに
+conv = kakasi.getConverter()
 
 with open(join(dirname(__file__), 'hightemp.txt'), 'r') as f:
     lines = f.readlines()
 
-prefecture = []
+pref_dict = {}
 for line in lines:
-    prefecture.append(line.split()[0])
+    _pref = line.split()[0]
+    pref_dict[_pref] = conv.do(_pref)
 
-prefecture = sorted(set(prefecture))
-print(prefecture)
+pref = []
+for k, v in sorted(pref_dict.items(), key=lambda x: x[1]):
+    pref.append(str(k))
+
+print('\n'.join(pref))
 
 # UNIX
 # cut -f 1 -d $' ' hightemp.txt | sort | uniq
