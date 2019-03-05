1bdb0fa0645936ab5d05a0eb89ede2ab1c5005b3
Masaki-Okuyama <cy16114@shibaura-it.ac.jp>
2019-03-05 19:21:01 +0900

    withの使用

--- octan/10.py
+++ octan/10.py
@@ -1,9 +1,9 @@
 # 行数をカウントせよ．確認にはwcコマンドを用いよ．
 
-temp_file = open("hightemp.txt", "r")
-print(len(temp_file.readlines()))
-# print(sum(1 for line in open("hightemp.txt", "r")))  #別解
-temp_file.close()
+with open("hightemp.txt", "r") as temp_file:
+    num_of_lines = len(temp_file.readlines())
+    # num_of_lines = sum(1 for line in open("hightemp.txt", "r"))  #別解
+    print(num_of_lines)
 
 # UNIXコマンド
 # wc -l hightemp.txt
--- octan/12.py
+++ octan/12.py
@@ -1,18 +1,25 @@
 # 各行の1列目だけを抜き出したものをcol1.txtに，2列目だけを抜き出したものをcol2.txtとしてファイルに保存せよ．
 # 確認にはcutコマンドを用いよ．
 
-temp_file = open("hightemp.txt", "r")
-col_file_1 = open("col1.txt", "w")
-col_file_2 = open("col2.txt", "w")
+col1_list = []
+col2_list = []
 
-for line in temp_file:
+with open("hightemp.txt", "r") as temp_file:
+    lines = temp_file.readlines()
+
+for line in lines:
     line_list = line.split()
-    col_file_1.write(line_list[0] + "\n")
-    col_file_2.write(line_list[1] + "\n")
+    col1_list.append(line_list[0])
+    col2_list.append(line_list[1])
 
-temp_file.close()
-col_file_1.close()
-col_file_2.close()
+col1_text = "\n".join(col1_list)
+col2_text = "\n".join(col2_list)
+
+with open("col1.txt", "w") as col1_file:
+    col1_file.write(col1_text)
+
+with open("col2.txt", "w") as col2_file:
+    col2_file.write(col2_text)
 
 # UNIXコマンド
 # cut -f 1 hightemp.txt > col1.txt
