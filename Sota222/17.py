482ca17755a54b0486cba9d46ef6772cfd6c2f8b
Sota222 <cy16179@shibauta-it.ac.jp>
2019-03-06 13:47:27 +0900

    読みやすい(と自分で思っている)コードに修正

--- Sota222/17.py
+++ Sota222/17.py
@@ -1,28 +1,33 @@
-# 1列目の文字列の種類（異なる文字列の集合）を求めよ．確認にはsort, uniqコマンドを用いよ．
+
+'''
+1列目の文字列の種類（異なる文字列の集合）を求めよ．確認にはsort, uniqコマンドを用いよ．
+'''
 
 # python
-import sys
-import numpy as np
-from pykakasi import kakasi
 from os.path import dirname, join
+from pykakasi import kakasi
+
+
+def main():
+    with open(join(dirname(__file__), 'hightemp.txt'), 'r') as f:
+        lines = f.readlines()
+
+    pref_dict = {}
+    for line in lines:
+        _pref = line.split()[0]
+        pref_dict[_pref] = conv.do(_pref)
+
+    pref = []
+    for k, _v in sorted(pref_dict.items(), key=lambda x: x[1]):
+        pref.append(str(k))
+    print('\n'.join(pref))
+
+
 # 漢字を平仮名にするconvを作成
 kakasi = kakasi()
 kakasi.setMode('J', 'H')  # Japanese(漢字)をasciiに
 conv = kakasi.getConverter()
 
-with open(join(dirname(__file__), 'hightemp.txt'), 'r') as f:
-    lines = f.readlines()
-
-pref_dict = {}
-for line in lines:
-    _pref = line.split()[0]
-    pref_dict[_pref] = conv.do(_pref)
-
-pref = []
-for k, v in sorted(pref_dict.items(), key=lambda x: x[1]):
-    pref.append(str(k))
-
-print('\n'.join(pref))
-
+main()
 # UNIX
 # cut -f 1 -d $' ' hightemp.txt | sort | uniq
