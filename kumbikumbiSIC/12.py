d4007dfe5cf1ddfa724c94300a5acbd47872eaa0
Hikaru Morita <tamori.iitomo12@gmail.com>
2019-03-06 22:06:44 +0900

    12 修正

--- kumbikumbiSIC/12.py
+++ kumbikumbiSIC/12.py
@@ -1,6 +1,7 @@
 '''
 1列目をcol1.txtに，2列目をcol2.txtに保存
-各行の1列目だけを抜き出したものをcol1.txtに，2列目だけを抜き出したものをcol2.txtとしてファイルに保存せよ．確認にはcutコマンドを用いよ．
+各行の1列目だけを抜き出したものをcol1.txtに，2列目だけを抜き出したものをcol2.txtとしてファイルに保存せよ．
+確認にはcutコマンドを用いよ．
 
 参考　12.pyの答え　	：　https://qiita.com/segavvy/items/51a515c19bcd29b13b7f
 参考　open()について	：　https://note.nkmk.me/python-file-io-open-with/
@@ -26,5 +27,13 @@ with open(file_name) as file, open("col1.txt", "w") as col1, \
 cut 参考url
 http://x68000.q-e-d.net/~68user/unix/pickup?cut
 
+diff 参考
+https://eng-entrance.com/linux-command-diff#i-8
 
+~/s/1/kumbikumbiSIC ❯❯❯ cut -f 1 hightemp.txt > col1_cut.txt                          kumbikumbiSIC
+~/s/1/kumbikumbiSIC ❯❯❯ cut -f 2 hightemp.txt > col2_cut.txt                          kumbikumbiSIC
+~/s/1/kumbikumbiSIC ❯❯❯ diff col1_cut.txt col1.txt                                    kumbikumbiSIC
+~/s/1/kumbikumbiSIC ❯❯❯ diff col2_cut.txt col2.txt                                    kumbikumbiSIC
+
+diffコマンドより差分がないことを確認
 '''
