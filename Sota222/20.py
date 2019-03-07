eb7e730fc12c61e38e751c3d9a606077e5a189ab
Sota222 <cy16179@shibauta-it.ac.jp>
2019-03-07 11:23:35 +0900

    最初の説明文追加

--- Sota222/20.py
+++ Sota222/20.py
@@ -1,6 +1,7 @@
-'''
-Wikipedia記事のJSONファイルを読み込み，「イギリス」に関する記事本文を表示せよ．問題21-29では，ここで抽出した記事本文に対して実行せよ．
-'''
+''' JSONデータの読み込み '''
+# Wikipedia記事のJSONファイルを読み込み，「イギリス」に関する記事本文を表示せよ．
+# 問題21-29では，ここで抽出した記事本文に対して実行せよ．
+
 import gzip
 import json
 from os.path import dirname, join
