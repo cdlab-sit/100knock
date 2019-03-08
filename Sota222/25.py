9d4da78de7cb277e650d62852a1343ab4779dad8
Sota222 <cy16179@shibauta-it.ac.jp>
2019-03-08 21:47:44 +0900

    つらかった...

--- Sota222/25.py
+++ Sota222/25.py
@@ -8,13 +8,11 @@ file_name = 'article_UK.json'
 with open(file_name, "r") as f:
     article_UK = json.load(f)
 
-basic_pattern = r'{{基礎情報(.+?)}}\n'
-basic_info_s = re.search(basic_pattern, article_UK, flags=re.DOTALL)
-print(basic_info_s.group())
-basic_info_str = str(basic_info_s)
-
 basic_pattern = r'\|(.+?) = (.+?)\n'
-basic_info_f = re.findall(basic_pattern, basic_info_s.group(), flags=re.DOTALL)
-print(basic_info_f)  # 最下のがない
-print()
+basic_info = re.findall(basic_pattern, article_UK, flags=re.DOTALL)
+basic_info_dic = {}
+for line in basic_info:
+    basic_info_dic[line[0]] = line[1]
 
+for k, v in basic_info_dic.items():
+    print(f'key = {k}, value = {v}')
