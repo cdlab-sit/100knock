e4c6af0f31da2badf02d44c193a48d056dfadcab
Sota222 <cy16179@shibauta-it.ac.jp>
2019-03-11 16:26:58 +0900

    Add 30_ready.py

---
+++ Sota222/30_ready.py
@@ -0,0 +1,36 @@
+''' 形態素解析 '''
+# 夏目漱石の小説『吾輩は猫である』の文章（neko.txt）をMeCabを使って形態素解析し，
+# その結果をneko.txt.mecabというファイルに保存せよ．
+# このファイルを用いて，以下の問に対応するプログラムを実装せよ．
+
+# なお，問題37, 38, 39はmatplotlibもしくはGnuplotを用いるとよい．
+
+
+def main():
+    """main文"""
+    in_file_name = 'neko.txt'
+    out_file_name = 'neko.txt.mecab'
+    neko_txt = read_file(in_file_name)
+    result = Morphological_analysis(neko_txt)
+    write_result(result, out_file_name)
+
+
+def read_file(file_name):
+    with open(file_name, 'r') as f:
+        contents = f.read()
+    return contents
+
+
+def Morphological_analysis(sentence):
+    import MeCab
+    t = MeCab.Tagger('')
+    return t.parse(sentence)
+
+
+def write_result(result, file_name):
+    with open(file_name, 'w') as f:
+        f.write(result)
+
+
+if __name__ == "__main__":
+    main()
