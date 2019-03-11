6376bb5c5421c344c7020107aa5fa7f5da598caa
Sota222 <cy16179@shibauta-it.ac.jp>
2019-03-11 17:51:49 +0900

    Add 30.py

---
+++ Sota222/30.py
@@ -0,0 +1,39 @@
+''' 形態素解析結果の読み込み '''
+# 態素解析結果（neko.txt.mecab）を読み込むプログラムを実装せよ．
+# ただし，各形態素は表層形（surface），基本形（base），品詞（pos），品詞細分類1（pos1）を
+# キーとするマッピング型に格納し，1文を形態素（マッピング型）のリストとして表現せよ．
+# 第4章の残りの問題では，ここで作ったプログラムを活用せよ．
+import re
+import pprint
+
+
+def main():
+    """main文"""
+    file_name = 'neko.txt.mecab'
+    contents = read_file(file_name)
+    result = load_contents(contents)
+    pprint.pprint(result)
+
+
+def read_file(file_name):
+    with open(file_name, 'r') as f:
+        contents = f.read()
+    return contents
+
+
+def load_contents(contents):
+    morphological = ['surface', 'base', 'pos', 'pos1']
+    element_place = [0, 7, 1, 2]
+    morphological_list = []
+    for line in contents.split('\n'):
+        if line == 'EOS':
+            break
+        splited_line = re.split('[\t,]', line)
+        morphological = {
+            morpho: splited_line[place]
+            for morpho, place in zip(morphological, element_place)}
+        morphological_list.append(morphological)
+    return morphological_list
+
+if __name__ == "__main__":
+    main()
