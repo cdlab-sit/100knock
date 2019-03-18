172375764ac6c0e3f9604476be17f35bb5a5bde6
Sota222 <cy16179@shibauta-it.ac.jp>
2019-03-18 20:07:01 +0900

    Add 42.py

---
+++ Sota222/42.py
@@ -0,0 +1,85 @@
+''' 係り元と係り先の文節の表示 '''
+# 係り元の文節と係り先の文節のテキストをタブ区切り形式ですべて抽出せよ．
+# ただし，句読点などの記号は出力しないようにせよ．
+import re
+
+
+def main():
+    file_name = 'neko.txt.cabocha'
+    neko_txt_cabocha = read_file(file_name)
+    sentence_list = load_dependency_parsing(neko_txt_cabocha)
+    write_dependency_relation(sentence_list)
+
+
+def read_file(file_name):
+    with open(file_name, 'r') as f:
+        contents = f.read()
+    return contents
+
+
+def load_dependency_parsing(analytical_data):
+    sentence_pattern = r"<sentence>(.+?)</sentence>"
+    chunk_pattern = r'<chunk id="(\d*)" link="(.+?)"(.+?)</chunk>'
+    morpheme_pattern = r'<tok id="\d*" feature="(.+?),(.+?),' \
+                       '(?:.+?),(.+?),(?:.+?)">(.+?)</tok>'
+    sentences = re.findall(sentence_pattern, analytical_data, flags=re.DOTALL)
+    sentence_list = []
+    for sentence in sentences:
+        chunks = re.findall(chunk_pattern, sentence, flags=re.DOTALL)
+        chunk_class_list = []
+        srcs_dic = {}
+        for chunk in chunks:
+            morphemes = re.findall(morpheme_pattern, chunk[2], flags=re.DOTALL)
+            morph_class_list = []
+            for morpheme in morphemes:
+                m = Morph(surface=morpheme[3], base=morpheme[2],
+                          pos=morpheme[0], pos1=morpheme[1])
+                morph_class_list.append(m)
+                srcs_dic.setdefault(int(chunk[0]), chunk[1])
+            srcs = [k for k, v in srcs_dic.items() if v == chunk[0]]
+            c = Chunk(morphs=morph_class_list, dst=chunk[1], srcs=srcs)
+            chunk_class_list.append(c)
+        sentence_list.append(chunk_class_list)
+    return sentence_list
+
+
+class Morph:
+    def __init__(self, surface, base, pos, pos1):
+        self.surface = surface
+        self.base = base
+        self.pos = pos
+        self.pos1 = pos1
+
+    def get_morph(self):
+        return f'surface = {self.surface}\tbase = {self.base}\t\
+pos = {self.pos}\tpos1 = {self.pos1}'
+
+
+class Chunk:
+    def __init__(self, morphs, dst, srcs):
+        self.morphs = morphs
+        self.dst = int(dst)
+        self.srcs = srcs
+
+    def get_phrase(self):
+        show_morphs = []
+        for morph in self.morphs:
+            if morph.pos != '記号':
+                show_morphs.append(morph.surface)
+        return ''.join(show_morphs)
+
+    def get_elements(self):
+        return f'morphs[{self.get_phrase()}]\t\
+dst[{self.dst}]\tsrcs = {self.srcs}'
+
+
+def write_dependency_relation(sentences):
+    f = open('dependency_relation.txt', 'w')
+    for chunks in sentences:
+        for chunk in chunks:
+            phrase = chunk.get_phrase()
+            if chunk.dst != -1 and phrase:
+                f.write(f'{phrase}\t{chunks[chunk.dst].get_phrase()}\n')
+    f.close()
+if __name__ == "__main__":
+    main()
