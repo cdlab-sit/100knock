12db321becdcb0ee3a6cf28e3a8e961ff87d1ec6
Sota222 <cy16179@shibauta-it.ac.jp>
2019-03-21 22:51:08 +0900

    動作確認完了

--- Sota222/p56.py
+++ Sota222/p56.py
@@ -9,16 +9,17 @@ import glob
 
 def main():
     file_name = '**/nlp.txt.xml'
-    names = replace_coreference(glob.glob(file_name, recursive=True)[0])
-    # show_doc(glob.glob(file_name, recursive=True)[0])
+    out_file_name = 'nlp.txt.changed.xml'
+    replace_coreference(glob.glob(file_name, recursive=True)[0])
+    show_doc(glob.glob(out_file_name, recursive=True)[0])
     # print(names)
 
 
 def replace_coreference(file_name):
     root = ET.parse(file_name).getroot()
+    tree = ET.ElementTree(element=root)
     coreference_iter = root[0].find('coreference')
     sentences = root[0].find('sentences')
-    print(sentences)
     for coreference in coreference_iter.findall('coreference'):
         re_mention = coreference[0].findtext('text')
         for mention_info in coreference[1:]:
@@ -26,16 +27,19 @@ def replace_coreference(file_name):
             sentence = int(mention_info.findtext('sentence'))
             start = int(mention_info.findtext('start'))
             end = int(mention_info.findtext('end'))
-            print(f'mention:{mention} sentence:{sentence} start:{start} end:{end}')
-            print(sentences[sentence-1][0][start:end].findtext('word'))
-        break
-    return 
+            # print(f're_mention:{re_mention} mention:{mention}')
+            for i in range(start, end-1):
+                sentences[sentence-1][0][i].clear()
+            if sentences[sentence-1][0][start-1]:
+                sentences[sentence-1][0][start-1].find('word').text = \
+                                f'[{re_mention}] ({mention})'
+    tree.write('nlp.txt.changed.xml')
 
 
 def show_doc(file_name):
 
     root = ET.parse(file_name).getroot()
-    sentences_iter = root.iter('sentence')
+    sentences_iter = root[0][1].iter('sentence')
     a = []
     for sentence in sentences_iter:
         a.append(' '.join([word.text for word in sentence.iter('word')]))
@@ -45,4 +49,6 @@ def show_doc(file_name):
 if __name__ == '__main__':
     main()
 
-# java -cp "*" -Xmx5g edu.stanford.nlp.pipeline.StanfordCoreNLP -annotators tokenize,cleanxml,ssplit,pos,lemma,ner,parse,dcoref -ssplit.eolonly -file nlp.txt 
+# java -cp "*" -Xmx5g edu.stanford.nlp.pipeline.StanfordCoreNLP
+# -annotators tokenize,cleanxml,ssplit,pos,lemma,ner,parse,dcoref
+# -ssplit.eolonly -file nlp.txt
