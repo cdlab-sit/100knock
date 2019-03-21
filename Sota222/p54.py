8816e5a637586a9de539a71bd55c9c7b96892ad3
Sota222 <cy16179@shibauta-it.ac.jp>
2019-03-21 15:53:19 +0900

    Add p54.py

---
+++ Sota222/p54.py
@@ -0,0 +1,28 @@
+''' 品詞タグ付け '''
+# Stanford Core NLPの解析結果XMLを読み込み，単語，レンマ，品詞をタブ区切り形式で出力せよ．
+from xml.etree import ElementTree as ET
+import glob
+
+
+def main():
+    file_name = '**/nlp.txt.xml'
+    pos_tag_word = read_xml(glob.glob(file_name, recursive=True)[0])
+    print(pos_tag_word)
+
+
+def read_xml(file_name):
+    tree = ET.parse(file_name)
+    root = tree.getroot()
+    words = [f'{token.findtext("word")}\t\
+{token.findtext("lemma")}\t{token.findtext("POS")}'
+             for token in root.iter('token')]
+    return '\n'.join(words)
+
+if __name__ == '__main__':
+    main()
+
+# コマンドライン上で操作するときについて
+# java -cp "*" -Xmx2g edu.stanford.nlp.pipeline.StanfordCoreNLP -annotators
+# tokenize,ssplit,pos,lemma,ner -file nlp.txt
+# というコマンドを使ったが、-annotatorsオプションがないと OutOfMemoryError: Java heap space
+# が発生。理由はわからん。
