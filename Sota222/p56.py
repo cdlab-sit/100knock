468f5943a061e12ee0b0a679b6424062c5e265ba
Sota222 <cy16179@shibauta-it.ac.jp>
2019-03-21 20:29:27 +0900

    どうループさせるか...

--- Sota222/p56.py
+++ Sota222/p56.py
@@ -9,16 +9,17 @@ import glob
 
 def main():
     file_name = '**/nlp.txt.xml'
-    # names = replace_coreference(glob.glob(file_name, recursive=True)[0])
-    show_doc(glob.glob(file_name, recursive=True)[0])
+    names = replace_coreference(glob.glob(file_name, recursive=True)[0])
+    # show_doc(glob.glob(file_name, recursive=True)[0])
     # print(names)
 
 
 def replace_coreference(file_name):
     root = ET.parse(file_name).getroot()
-    for coreferences in root.iter('coreference'):
-        print(coreferences[0].findtext('end'))
-
+    coreference_iter = root.find('document').find('coreference')
+    for coreferences in coreference_iter.findall('coreference'):
+        print(coreferences[0])
+        print('a')
     return 
 
 
@@ -26,20 +27,13 @@ def show_doc(file_name):
 
     root = ET.parse(file_name).getroot()
     sentences_iter = root.iter('sentence')
+    a = []
     for sentence in sentences_iter:
-        for word in sentence.iter('word'):
-            print(word.text)
-        #sentence
-    # print(sentence.attrib)
-        # print(token.findtext('word'))
-        print('----------')
+        a.append(' '.join([word.text for word in sentence.iter('word')]))
 
+    print('\n\n'.join(a))
 
 if __name__ == '__main__':
     main()
-
-# java -cp "*" -Xmx2g edu.stanford.nlp.pipeline.StanfordCoreNLP -annotators tokenize,cleanxml,ssplit -ssplit.eolonly,pos,lemma,ner,parse,dcoref -file nlp.txt -tokenize.whitespace -ssplit.eolonly
-
-# java -cp "*" -Xmx2g edu.stanford.nlp.pipeline.StanfordCoreNLP -annotators tokenize,cleanxml,ssplit,pos,lemma,ner parse,dcoref -ssplit.eolonly -file nlp.txt 
 
 # java -cp "*" -Xmx5g edu.stanford.nlp.pipeline.StanfordCoreNLP -annotators tokenize,cleanxml,ssplit,pos,lemma,ner,parse,dcoref -ssplit.eolonly -file nlp.txt 
