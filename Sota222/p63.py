2f31c9363e191d0cf140161dfa97e7ecab72f5cd
Sota222 <cy16179@shibauta-it.ac.jp>
2019-03-23 14:55:07 +0900

    もうちょい

--- Sota222/p63.py
+++ Sota222/p63.py
@@ -9,29 +9,33 @@ import json
 
 def main():
     file_name = 'artist.json.gz'
-    set_tag_db(file_name)
-    artist = input('検索するアーティスト名を入力してください')
-    search_tag(artist)
+    artist_db = set_tag_db(file_name)
+    # artist = input('検索するアーティスト名を入力してください\n:')
+    artist = 'Oasis'
+    search_tag(artist_db, artist)
+
 
 def set_tag_db(file_name):
     artist_db = plyvel.DB('artist_tags_db', create_if_missing=True)
     with gzip.open(file_name, "r") as f_file:
         for line in f_file:
             data = json.loads(line)
-            if 'tag' in data:
+            if 'name' in data and 'tags' in data:
                 tags = json.dumps(data.get('tags'))
-                artist_db.put(data.get('name').encode(), tags.encode())
+                artist_db.put(data.get('name').encode('utf-8'),
+                              tags.encode('utf-8'))
     artist_db.close()
+    return 'artist_tags_db'
 
 
-def search_tag(artist):
-    artist_db = plyvel.DB('artist_tags_db', create_if_missing=True)
-    for k, v in artist_db.iterator():
+def search_tag(tags_db, artist):
+    db = plyvel.DB(tags_db, create_if_missing=True)
+    for k, v in db.iterator():
         if k == artist.encode():
             tags = json.loads(v.decode())
-        for tag in tags:
-            print(tag)
-    artist_db.close()
+            for tag in tags:
+                print(tag)
+    db.close()
 
 
 if __name__ == '__main__':
